2784ec32c7cb7fbae5de1bffa367981806c4a33c
Akila_Lakshan_weerathunga <akila@portion.lk>
2024-06-28 00:36:33 +0530

    update

--- admin/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connect = new PDO("mysql:host=localhost; dbname=mammypages_mummy", "mammypages_mummypages", "T.EgWfWm)US3");
-$conn =mysqli_query("localhost", "mammypages_mummypages", "T.EgWfWm)US3", "mammypages_mummy");
-?>
