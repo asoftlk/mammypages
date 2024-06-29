fa4a933be3c2c4057a6c1f4901502d000e72f5b9
Akila_Lakshan_weerathunga <akila@portion.lk>
2024-06-29 09:23:39 +0530

    sidebar update

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,13 @@
-<div class="left-menu-part">
-    <h3>MP Directory</h3>
-    <ul style="list-style-type:none; padding-left:0">
-    <li><a role="button" href="hospital"><i class="icofont-hospital"></i> HOSPITALS</a></li>
-    <li><a role="button" href="doctors"><i class="icofont-doctor"></i> DOCTORS</a></li>
-    <li><a role="button" href="midwifes"><i class="icofont-nurse"></i> MIDWIFE CLINICS</a></li>
-    <li><a role="button" href="medical"><i class="icofont-nurse-alt"></i> MEDICAL CLINICS</a></li>
-    <li><a role="button" href="pharmacies"><i class="icofont-medical-sign"></i> PHARMACIES</a></li>
-    <li><a role="button" href="beauty"><i class="icofont-girl-alt"></i> BEAUTY SALON</a></li>
-    </ul>
-    <div class="client-sec"><a class="client-btn">Sponsors</a></div>
-</div>
+
+<h3>MP Directory</h3>
+<ul style="list-style-type:none; padding-left:0">
+<li><a role="button" href="hospital"><i class="icofont-hospital"></i> HOSPITALS</a></li>
+<li><a role="button" href="doctors"><i class="icofont-doctor"></i> DOCTORS</a></li>
+<li><a role="button" href="midwifes"><i class="icofont-nurse"></i> MIDWIFE CLINICS</a></li>
+<li><a role="button" href="medical"><i class="icofont-nurse-alt"></i> MEDICAL CLINICS</a></li>
+<li><a role="button" href="pharmacies"><i class="icofont-medical-sign"></i> PHARMACIES</a></li>
+<li><a role="button" href="beauty"><i class="icofont-girl-alt"></i> BEAUTY SALON</a></li>
+</ul>
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
